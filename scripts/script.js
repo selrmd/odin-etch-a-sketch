aa8bebd8eeaf4fe9c514ed8e8cadad8f23aa1cec
selma <sam.learntocode@gmail.com>
2022-08-28 17:29:02 +0100

    Create the grid with a two-dimensional array
    Just added the divs to the grid, no styling was done

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+// create the grid using two-dimensional array
+// with a fixed 16 div for now
+const row = 16, column = 16;
+
+// initialize the array
+const gridArray = new Array(row);
+for(let i = 0; i < row; i++){
+    gridArray[i] = new Array(column);
+}
+
+// populate the grid with divs
+for(let i = 0; i < row; i++){
+    for(let j = 0; j < column; j++){
+        gridArray[i][j] = document.getElementById('grid-container')
+        .appendChild(document.createElement('div'));
+    }
+
+}
+
+// add a class to the array for testing
+for(let i = 0; i < row; i++){
+    for(let j = 0; j < column; j++){
+        gridArray[i][j].classList.add('grid-box');
+    }
+}
